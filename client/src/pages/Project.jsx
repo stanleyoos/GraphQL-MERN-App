b47744bd6c99a18ee602b37435430a9abe065040
stanleyos <stanislaw.sarka@gmail.com>
2023-10-06 10:27:25 +0200

    feat- created Project comoponent to display single project && GET_PROJECT query

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,11 +1,40 @@
 import { useParams } from "react-router-dom"
+import { useQuery } from "@apollo/client"
+import { GET_PROJECT } from "../queries/projectQueries"
+import Spinner from "../components/Spinner"
+import { Link } from "react-router-dom"
 
 const Project = () => {
   const { id } = useParams()
+  const { loading, error, data } = useQuery(GET_PROJECT, {
+    variables: { id },
+  })
+
+  if (loading) return <Spinner />
+  if (error) return <p>Error!</p>
   return (
-    <div>
-      <h1>Project with ID: {id}</h1>
-    </div>
+    <>
+      {!loading && !error && (
+        <div className="d-flex justify-content-center mt-5">
+          <div class="card p-3" style={{ width: "28rem" }}>
+            <div class="card-body">
+              <div className="d-flex justify-content-between">
+                <h1 className="mb-4">{data.project.name}</h1>
+                <Link to={`/`}>
+                  <button className="btn btn-light" style={{ height: "40px" }}>
+                    Get back
+                  </button>
+                </Link>
+              </div>
+              <p class="card-text mb-4">{data.project.description}</p>
+              <p class="card-text">
+                Status: <strong>{data.project.status}</strong>
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
